264a98a61dd1f635f6513dd00e7c051ef0147589
Gabor Nyeki <gabor.nyeki@alumni.duke.edu>
2023-04-12 23:34:22 -0400

    let specification override `padding-bottom = "1em"` for 'coef'

--- tomltable/tomltable.py
+++ tomltable/tomltable.py
@@ -327,7 +327,7 @@ def make_rows_for_cell_spec_regression(spec, column_count):
     custom_spec = {
         "label": spec.get("label", ""),
         "cell": cell_values,
-        "padding-bottom": "1em"
+        "padding-bottom": spec.get("padding-bottom", "1em")
     }
 
     return make_rows_for_cell_spec_custom(custom_spec, column_count)
