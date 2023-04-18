8162cd4398d1071f6d0e06542645e388a311b936
Gabor Nyeki <gabor.nyeki@alumni.duke.edu>
2023-04-18 13:19:56 -0400

    setup.py: bump version number to 1.1

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup
 
 setup(
     name='tomltable',
-    version='1.0',
+    version='1.1',
     description=u'command-line tool to generate TOML-defined regression tables from JSON data files',
     author=u'Gabor Nyeki',
     url='https://www.gabornyeki.com/',
     packages=['tomltable'],
     install_requires=['click', 'toml'],
-    provides=['tomltable (1.0)'],
+    provides=['tomltable (1.1)'],
     entry_points={
         'console_scripts': [
             'tomltable = tomltable:main',
